2760aea902970f0bdfe2e1cb5200088f6e074ddf
Ward <wardv@tirf.ca>
2022-02-09 15:25:50 -0500

    created database

--- employee-tracker.sql
+++ employee-tracker.sql
@@ -1,3 +1,33 @@
+ DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
+
 INSERT INTO departments (id, name)
 VALUES
   (1, 'admin&finance'),
