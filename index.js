80675cda5b0354965153a25b18d414c3358ba4e9
Ward <wardv@tirf.ca>
2022-02-10 16:40:50 -0500

    began working on adding role

--- index.js
+++ index.js
@@ -46,49 +46,94 @@ const promptUser = () => {
       if (options === "view all departments") {
         db.query(`SELECT * FROM departments`, function (err, results) {
           console.table("Departments", results);
+          promptUser();
         });
       } else if (options === "view all roles") {
         db.query(`SELECT * FROM roles`, function (err, results) {
           console.table("Roles", results);
+          promptUser();
         });
       } else if (options === "view all employees") {
         db.query(`SELECT * FROM employees`, function (err, results) {
           console.table("Employees", results);
+          promptUser();
         });
       } else if (options === "add a department") {
-        return (
-          inquirer
-            .prompt([
-              {
-                type: "input",
-                name: "title",
-                message: "What is the title of the department? (Required)",
-                validate: (titleInput) => {
-                  if (titleInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter a title!");
-                    return false;
-                  }
-                },
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What is the title of the department? (Required)",
+              validate: (titleInput) => {
+                if (titleInput) {
+                  return true;
+                } else {
+                  console.log("Please enter a title!");
+                  return false;
+                }
               },
-            ])
-            // .then(({ title }) => {
-            //   db.query(
-            //     `INSERT INTO departments (name) VALUES (${title})`,
-            //     function (err, results) {
-            //       console.log(results);
-            //     }
-            //   );
-            // });
+            },
+          ])
+          .then(({ title }) => {
+            const sql = `INSERT INTO departments (name) VALUES (?)`;
+            db.query(sql, title, function (err, results) {
+              console.log(`Department added`);
+              promptUser();
+            });
+          });
+      } else if (options === "add a role") {
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What is the title of the role would you like to add? (Required)",
+              validate: (titleInput) => {
+                if (titleInput) {
+                  return true;
+                } else {
+                  console.log("Please enter a role!");
+                  return false;
+                }
+              },
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "What salary does this role make?",
+              validate: (salaryInput) => {
+                if (isNaN(salaryInput)) {
+                  console.log("Please enter a salary using numbers only");
+                  return false;
+                } else {
+                  return true;
+                }
+              },
+            },
+            {
+              type: "input",
+              name: "department",
+              message: "What department does this role belong to? Use the corresponding number ID from the departments table.
+              validate: (departmentInput) => {
+                if (isNaN(departmentInput)) {
+                  console.log("Please enter a department using its corresponding number");
+                  return false;
+                } else {
+                  return true;
+                }
+              },
+            },
+          ])
+          .then(roleData => {
+            let {title, salary, department} = roleData
 
-            .then(({ title }) => {
-              const sql = `INSERT INTO departments (name) VALUES (?)`;
-              db.query(sql, title, function (err, results) {
-                console.log(`Department added`);
-              });
-            })
-        );
+            const sql = `INSERT INTO departments (title, salary, department_id) VALUES (${title}, ${salary}, ${department})`;
+            db.query(sql, roleData, function (err, results) {
+              console.log(results);
+              promptUser();
+            });
+          });
       }
     });
 };
